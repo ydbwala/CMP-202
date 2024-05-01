3d30e9e6503978cb3d9b42519aee6cf21856b999
Yati Bwala <desmondbwala@gmail.com>
2024-05-01 12:31:15 +0100

    oop classwork

---
+++ Admin.java
@@ -0,0 +1,12 @@
+public class Admin extends Userr {
+
+
+    String staffNumber;
+    StrictMath uploadResult( String course, float score){
+      course = "CMP202";
+    score = 6;
+      return uploadResult(course, score);
+    }
+
+
+}
---
+++ Student.java
@@ -0,0 +1,29 @@
+public class Student extends Userr{
+
+    String matricNo;
+   String AdmissionNo;
+
+    String RegisterCourse(){
+        return "CMP202";
+    }
+    
+    String CheckResult(){
+     matricNo = "BHU/22/04/09/0039";
+     AdmissionNo = "112242558";
+     if (matricNo.equals("BHU/22/04/09/0039")||AdmissionNo.equals("112242558")) {
+        
+        return "Result";
+     }
+
+     else{
+        return "Input matric number and admission number";
+     }
+
+     
+    }
+    public static void main(String[] args) {
+        
+        
+        System.out.println();
+    }
+}
